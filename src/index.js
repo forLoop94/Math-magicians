c94cece6ec74740cecb863a720cdbf938ba7628d
forLoop94 <charlesuwem94@gmail.com>
2023-05-30 15:44:04 +0100

    Delete commented snippets of code in index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
 import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
