07f5bd5eee4acc5fdac7c377f66d15a1da26c799
forLoop94 <charlesuwem94@gmail.com>
2023-06-01 10:50:52 +0100

    Add event listeners and handlers to calculator keys and test them on the console

--- src/components/CalculatorKeys.js
+++ src/components/CalculatorKeys.js
@@ -1,25 +1,32 @@
-const CalculatorKeys = () => (
-  <div className="keys-container">
-    <button type="button" className="keys left-border">AC</button>
-    <button type="button" className="keys">+/-</button>
-    <button type="button" className="keys">%</button>
-    <button type="button" className="keys orange-keys">÷</button>
-    <button type="button" className="keys">7</button>
-    <button type="button" className="keys">8</button>
-    <button type="button" className="keys">9</button>
-    <button type="button" className="keys orange-keys">X</button>
-    <button type="button" className="keys">4</button>
-    <button type="button" className="keys">5</button>
-    <button type="button" className="keys">6</button>
-    <button type="button" className="keys orange-keys">-</button>
-    <button type="button" className="keys">1</button>
-    <button type="button" className="keys">2</button>
-    <button type="button" className="keys">3</button>
-    <button type="button" className="keys orange-keys">+</button>
-    <button type="button" className="keys span-2">0</button>
-    <button type="button" className="keys">.</button>
-    <button type="button" className="keys orange-keys">=</button>
-  </div>
-);
+// import calculate from '../logic/calculate';
+
+const CalculatorKeys = () => {
+  const keyHandler = (e) => {
+    console.log(e.target.innerText);
+  };
+  return (
+    <div className="keys-container">
+      <button onClick={keyHandler} type="button" className="keys left-border">AC</button>
+      <button onClick={keyHandler} type="button" className="keys">+/-</button>
+      <button onClick={keyHandler} type="button" className="keys">%</button>
+      <button onClick={keyHandler} type="button" className="keys orange-keys">÷</button>
+      <button onClick={keyHandler} type="button" className="keys">7</button>
+      <button onClick={keyHandler} type="button" className="keys">8</button>
+      <button onClick={keyHandler} type="button" className="keys">9</button>
+      <button onClick={keyHandler} type="button" className="keys orange-keys">X</button>
+      <button onClick={keyHandler} type="button" className="keys">4</button>
+      <button onClick={keyHandler} type="button" className="keys">5</button>
+      <button onClick={keyHandler} type="button" className="keys">6</button>
+      <button onClick={keyHandler} type="button" className="keys orange-keys">-</button>
+      <button onClick={keyHandler} type="button" className="keys">1</button>
+      <button onClick={keyHandler} type="button" className="keys">2</button>
+      <button onClick={keyHandler} type="button" className="keys">3</button>
+      <button onClick={keyHandler} type="button" className="keys orange-keys">+</button>
+      <button onClick={keyHandler} type="button" className="keys span-2">0</button>
+      <button onClick={keyHandler} type="button" className="keys">.</button>
+      <button onClick={keyHandler} type="button" className="keys orange-keys">=</button>
+    </div>
+  );
+};
 
 export default CalculatorKeys;
