33b90d548f099e2426dabedb7ceb468888ca0d0c
forLoop94 <charlesuwem94@gmail.com>
2023-06-06 21:18:52 +0100

    Re-style calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
-import DisplayQuote from './components/DisplayQuotes';
 import './App.css';
 import Quote from './pages/Quote';
 import Home from './pages/Home';
 
 const App = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={(
-        <main>
-          <DisplayQuote />
-          <Calculator />
-        </main>
-      )}
-    />
-    <Route path="/home" element={<Home />} />
-    <Route path="/quote" element={<Quote />} />
-  </Routes>
+  <>
+    <nav className="flex">
+      <h1>Math Magicians</h1>
+      <ul className="flex">
+        <li>
+          <Link className="links" to="/home">Home |</Link>
+        </li>
+        <li>
+          <Link className="links" to="/">Calculator |</Link>
+        </li>
+        <li>
+          <Link className="links" to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <main className="flex">
+            <p>Lets do some math!</p>
+            <div>
+              <Calculator />
+            </div>
+          </main>
+        )}
+      />
+      <Route path="/home" element={<Home />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
