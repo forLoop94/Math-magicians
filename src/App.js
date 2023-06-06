bae9340cec33d4fb19ccd5b33ecf88d050eb92d3
forLoop94 <charlesuwem94@gmail.com>
2023-06-06 19:52:09 +0100

    Add Route for home and quote pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import DisplayQuote from './components/DisplayQuotes';
 import './App.css';
+import Quote from './pages/Quote';
+import Home from './pages/Home';
 
 const App = () => (
-  <main>
-    <DisplayQuote />
-    <Calculator />
-  </main>
+  <Routes>
+    <Route
+      path="/"
+      element={(
+        <main>
+          <DisplayQuote />
+          <Calculator />
+        </main>
+      )}
+    />
+    <Route path="/home" element={<Home />} />
+    <Route path="/quote" element={<Quote />} />
+  </Routes>
 );
 
 export default App;
